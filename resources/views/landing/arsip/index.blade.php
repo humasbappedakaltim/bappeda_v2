5cde4c10169c33ba33a48a5f412d670de35396b3
andypratama3 <andypratama1211@gmail.com>
2025-01-12 22:13:09 +0800

    feat: storgae udpate

--- resources/views/landing/arsip/index.blade.php
+++ resources/views/landing/arsip/index.blade.php
@@ -59,7 +59,7 @@
                 $routeName = $routeMap[$categoryName] ?? 'berita.show';
                 $url = route($routeName, ['slug' => $post->slug]);
             @endphp
-            <div class="col-md-4 m-2 p-0 w-30">
+            <div class="col-md-4 m-2 p-0">
                 <a href="{{ $url }}" class="text-decoration-none text-black">
                     <div class="card border-0 shadow-sm mb-3 card-hover">
                         <div class="card-body">
