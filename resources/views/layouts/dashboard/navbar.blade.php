6feafe2d4611ee984c19b15991cc3841bcf15704
andypratama3 <andypratama1211@gmail.com>
2025-01-13 02:27:42 +0800

    feat: update navbar

--- resources/views/layouts/dashboard/navbar.blade.php
+++ resources/views/layouts/dashboard/navbar.blade.php
@@ -165,13 +165,13 @@
             <div class="dropdown-menu dropdown-menu-end">
                 <a class="dropdown-item" href="{{ route('profile.show') }}"><i class="align-middle me-1"
                         data-feather="user"></i> Profile</a>
-                <a class="dropdown-item" href="#"><i class="align-middle me-1"
+                {{-- <a class="dropdown-item" href="#"><i class="align-middle me-1"
                         data-feather="pie-chart"></i> Analytics</a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="index.html"><i class="align-middle me-1"
                         data-feather="settings"></i> Settings & Privacy</a>
                 <a class="dropdown-item" href="#"><i class="align-middle me-1"
-                        data-feather="help-circle"></i> Help Center</a>
+                        data-feather="help-circle"></i> Help Center</a> --}}
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" id="logout">Log out
                     <form action="{{ route('logout') }}" method="post" id="logout-form">
