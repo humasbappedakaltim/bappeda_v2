743fa8fd0afc70153fb2f835832b05f2c98f2feb
andypratama3 <andypratama1211@gmail.com>
2025-01-15 23:44:14 +0800

    update

--- resources/views/landing/ppid/dasar_hukum.blade.php
+++ resources/views/landing/ppid/dasar_hukum.blade.php
@@ -7,7 +7,7 @@
     <section class="breadcrumb-section position-relative d-flex justify-content-center">
         <div class="container custom-container m-0 px-4 py-0">
             <div class="breadcrumb-container d-flex align-items-center gap-2 fs-8 mb-3">
-                <a href="index.html" class="breadcrumb-link text-decoration-none fw-500 m-0 p-0">Beranda</a>
+                <a href="{{ route('ppid.index') }}" class="breadcrumb-link text-decoration-none fw-500 m-0 p-0">Beranda</a>
                 <i class="bi bi-chevron-right text-white m-0 p-0"></i>
                 <p class="breadcrumb-active text-white fw-500 m-0 p-0">Dasar Hukum PPID BAPPEDA</p>
             </div>
