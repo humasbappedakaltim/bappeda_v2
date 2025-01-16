019226ad341a1bfb928dcc0092dd311b63bd24c4
andypratama3 <andypratama1211@gmail.com>
2025-01-16 22:27:55 +0800

    update CRUD quil and show post

--- resources/views/dashboard/post/show.blade.php
+++ resources/views/dashboard/post/show.blade.php
@@ -1,101 +1,73 @@
 @extends('layouts.dashboard')
-@section('title','Lihat Postingan')
+
+@section('title', 'Detail Postingan')
+
 @push('css')
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.snow.css" rel="stylesheet" />
-
 @endpush
+
 @section('content')
-<h1 class="h3 mb-3">
-    <strong>Lihat Postingan {{ $post->title }}</strong>
-</h1>
+<h1 class="h3 mb-3"><strong>Detail Postingan</strong></h1>
 
 <div class="row">
-    <div class="col-md-12 d-flex">
-        <div class="w-100">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <form>
-                                @csrf
-                                <div class="row mb-3">
-                                    <div class="col-md-12 mt-2">
-                                        <label for="title" class="form-label">Judul</label>
-                                        <input type="text" class="form-control" id="title" name="title" value="{{ $post->title }}" readonly>
-                                    </div>
-                                    <div class="col-md-6 mt-2" >
-                                        <label for="category_id" class="form-label">Kategori</label>
-                                        <select name="category_id" id="category_id" class="form-control" disabled>
-                                            <option value="">Pilih Kategori</option>
-                                                <option value="{{ $post->category_id }}" {{ $post->category_id == $post->category_id ? 'selected' : '' }}>{{ $post->category->name }}</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-md-6 mt-2">
-                                        <label for="user_id" class="form-label">Pembuat</label>
-                                        <select name="user_id" id="user_id" class="form-control" disabled>
-                                            <option value="{{ $post->user ? $post->user->id : '' }}" {{ $post->user && $post->user->id == $post->user_id ? 'selected' : '' }}>{{ $post->user ? $post->user->name : '' }}</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-md-6 mb-2">
-                                        <label for="tags" class="form-label">tags</label>
-                                        <input type="text" class="form-control" id="tags" name="tags" value="{{ $post->tags }}" readonly>
-                                    </div>
-                                    <div class="col-md-6 mb-2">
-                                        <label for="keywords" class="form-label">Keywords</label>
-                                        <input type="text" class="form-control" id="keywords" name="keywords" value="{{ $post->keywords }}" readonly>
-                                    </div>
-                                    <div class="col-md-12 mb-2">
-                                        <label for="status" class="form-label">Status Publish</label>
-                                        <select name="status" id="status" class="form-control" disabled>
-                                            <option selected>Pilih Status</option>
-                                            <option value="0" {{ $post->status == '0' ? 'selected' : '' }}>Draf</option>
-                                            <option value="1" {{ $post->status == '1' ? 'selected' : '' }}>Publish</option>
+    <div class="col-md-12">
+        <div class="card">
+            @include('layouts.flashmessage')
+            <div class="card-body">
+                <!-- Title -->
+                <div class="mb-3">
+                    <label class="form-label">Judul</label>
+                    <input type="text" class="form-control" value="{{ $post->title }}" readonly>
+                </div>
 
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-md-12">
-                                        <label for="image" class="form-label">Gambar</label>
-                                        <img src="{{ asset('storage/post/'.$post->image) }}" class="img-preview img-fluid mb-3" style="border-radius: 10px;">
-                                    </div>
-                                </div>
-                                <div class="row mb-7">
-                                    <div class="col-md-12 mb-4">
-                                        <label for="description" class="form-label">Deskripsi</label>
-                                        <div id="editor">
-                                            {!! $post->description !!}
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row mb-5 mt-4">
-                                    <div class="col-md-12 mb-4">
-                                        <label for="extra_photo" class="form-label">Foto Tambahan</label>
-                                        <div class="row">
-                                            @php
-                                                $extra_photos = explode(',', $post->extra_photo);
-                                            @endphp
-                                            @foreach ($extra_photos as $key => $extra_photo)
-                                                <div class="col-md-4">
-                                                    <img src="{{ asset('storage/post/extra/'. $extra_photo) }}" class="img-preview img-fluid mb-3" style="border-radius: 10px;">
-                                                </div>
-                                            @endforeach
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row mt-5">
-                                    <div class="col-md-12 mt-5">
-                                        <a href="{{ route('dashboard.post.news.index') }}" class="btn btn-danger">Kembali</a>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                <!-- Category and Creator -->
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <label class="form-label">Kategori</label>
+                        <input type="text" class="form-control" value="{{ $post->category->name }}" readonly>
                     </div>
+                    <div class="col-md-6">
+                        <label class="form-label">Pembuat</label>
+                        <input type="text" class="form-control" value="{{ $post->user->name }}" readonly>
+                    </div>
+                </div>
+
+                <!-- Tags and Keywords -->
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <label class="form-label">Tags</label>
+                        <input type="text" class="form-control" value="{{ $post->tags }}" readonly>
+                    </div>
+                    <div class="col-md-6">
+                        <label class="form-label">Keywords</label>
+                        <input type="text" class="form-control" value="{{ $post->keywords }}" readonly>
+                    </div>
+                </div>
+
+                <!-- Status -->
+                <div class="mb-3">
+                    <label class="form-label">Status Publish</label>
+                    <input type="text" class="form-control" value="{{ $post->status == '1' ? 'Publish' : 'Draf' }}" readonly>
+                </div>
+
+                <!-- Image -->
+                <div class="mb-3">
+                    <label class="form-label">Gambar</label>
+                    <div class="mt-3 text-center">
+                        <img src="{{ $post->image ? asset('storage/post/' . $post->image) : '' }}" id="output" class="img-preview img-fluid" style="border-radius: 10px;">
+                    </div>
+                </div>
+
+                <!-- Description -->
+                <div class="mb-3">
+                    <label class="form-label">Deskripsi</label>
+                    <div>{!! $post->description !!}</div>
+                </div>
+
+                <!-- Back Button -->
+                <div class="mt-5">
+                    <a href="{{ route('dashboard.post.news.index') }}" class="btn btn-danger">Kembali</a>
                 </div>
             </div>
         </div>
@@ -104,74 +76,28 @@
 
 @push('js')
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.js"></script>
+    <script src="https://cdn.bootcdn.net/ajax/libs/quill/1.3.7/quill.min.js"></script>
+    <script defer src="https://unpkg.com/quill-resize-image@1.0.5/dist/quill-resize-image.min.js"></script>
 
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $('.select2').select2();
-            const toolbarOptions = [
-                ['bold', 'italic', 'underline', 'strike'],
-                ['blockquote', 'code-block'],
+            Quill.register("modules/resize", window.QuillResizeImage);
 
-                [{
-                    'header': 1
-                }, {
-                    'header': 2
-                }], // custom button values
-                [{
-                    'list': 'ordered'
-                }, {
-                    'list': 'bullet'
-                }],
-                [{
-                    'script': 'sub'
-                }, {
-                    'script': 'super'
-                }], // superscript/subscript
-                [{
-                    'indent': '-1'
-                }, {
-                    'indent': '+1'
-                }], // outdent/indent
-                [{
-                    'direction': 'rtl'
-                }], // text direction
-
-                [{
-                    'size': ['small', false, 'large', 'huge']
-                }], // custom dropdown
-                [{
-                    'header': [1, 2, 3, 4, 5, 6, false]
-                }],
-
-                [{
-                    'color': []
-                }, {
-                    'background': []
-                }],
-                [{
-                    'font': []
-                }],
-                [{
-                    'align': []
-                }],
-
-                ['clean']
-            ];
-
-            var quill = new Quill('#editor', {
+            const quill = new Quill('#editor', {
                 modules: {
-                    toolbar: toolbarOptions
+                    toolbar: false,
+                    resize: {
+                        locale: {},
+                    },
                 },
-                theme: 'snow'
+                theme: 'snow',
+                readOnly: true
             });
-            quill.enable(false);
+
+            quill.root.innerHTML = '{!! $post->description !!}';
         });
     </script>
-
-
 @endpush
 @endsection
-
-
 
