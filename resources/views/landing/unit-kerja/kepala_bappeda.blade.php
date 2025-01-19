eb433274f028a278a5d201b9bc424d21210b92a5
andypratama3 <andypratama1211@gmail.com>
2025-01-20 05:22:34 +0800

    translate page: Asn

--- resources/views/landing/unit-kerja/kepala_bappeda.blade.php
+++ resources/views/landing/unit-kerja/kepala_bappeda.blade.php
@@ -10,9 +10,11 @@
                 <a href="{{ route('landing.index') }}" class="breadcrumb-link text-capitalize text-decoration-none fw-500 m-0 p-0">{{ translate('beranda') }}</a>
                 <i class="bi bi-chevron-right text-white m-0 p-0"></i>
                 <p class="breadcrumb-active text-white fw-500 m-0 p-0">{{ translate('Kepala BAPPEDA') }}</p>
-            <div class="title-content">
-                <h4 class="title fw-bold mb-3">
-                    {{ translate('Kepala BAPPEDA') }}
+                <div class="title-content">
+                    <h4 class="title fw-bold mb-3">
+                        {{ translate('Kepala BAPPEDA') }}
+                    </h4>
+                </div>
             </div>
         </div>
     </section>
@@ -29,14 +31,14 @@
                 <div class="col-12 px-0">
                     <p class="title text-uppercase fs-4 fw-bold">
                         {{ translate('kepala bappeda') }}
-                        kepala bappeda
+
                     <hr class="hr-title-black flex-grow-1 mb-5">
                     <p class="fs-8 fw-bold mb-2">{{ translate('Ikhtisar Jabatan') }} :</p>
                     <p class="text-justify fs-8 fw-light lh-sm mb-4">{{ translate('Memimpin dan melaksanakan penyelenggaraan urusan pemerintahan dan pelayanan Organisasi Perangkat Daerah yang menjadi kewenangan Badan Perencanaan Pembangunan Daerah Provinsi Kalimantan Timur terkait pelaksanaan tugas bidang perencanaan pembangunan daerah yang menjadi kewenangannya sesuai dengan peraturan dan ketentuan yang berlaku agar pelaksanaan tugas berjalan lancar, efektif dan efisien.') }}</p>
                     <p class="fs-8 fw-bold mb-2">{{ translate('Tugas Pokok') }} :</p>
                     <p class="text-justify fs-8 fw-light lh-sm mb-4">{{ translate('Membantu Gubernur melaksanakan Urusan Pemerintahan yang menjadi kewenangan daerah di bidang perencanaan.') }}</p>
                     <p class="fs-8 fw-bold mb-2">{{ translate('Uraian Tugas') }} :</p>
-                    <p class="fs-8 fw-bold mb-2">Ikhtisar Jabatan :</p>
+
                         <li class="text-justify fs-8 fw-light lh-sm mb-1">{{ translate('Merumuskan program kerja di lingkungan Badan Perencanaan Pembangunan Daerah Provinsi Kalimantan Timur berdasarkan rencana Strategis Bappeda Provinsi Kalimantan Timur sebagai pedoman pelaksanaan tugas.') }}</li>
                         <li class="text-justify fs-8 fw-light lh-sm mb-1">{{ translate('Mengkoordinasikan pelaksanaan tugas di lingkungan Badan Perencanaan Pembanguan Daerah Provinsi Kalimantan Timur sesuai dengan program yang telah ditetapkan dan sesuai dengan kebijakan pimpinan agar target kerja tercapai sesuai rencana.') }}</li>
                         <li class="text-justify fs-8 fw-light lh-sm mb-1">{{ translate('Membina bawahan di lingkungan Badan Perencanaan Pembanguan Daerah Provinsi Kalimantan Timur dengan cara mengadakan rapat/ pertemuan dan bimbingan secara berkala agar diperoleh kinerja yang diharapkan. ') }}</li>
@@ -48,7 +50,6 @@
                         <li class="text-justify fs-8 fw-light lh-sm mb-4">{{ translate('Menyusun laporan pelaksanaan tugas di lingkungan Badan Perencanaan dan Pembangunan Daerah sesuai dengan kegiatan yang telah dilaksanakan sebagai akuntabilitas kinerja Badan.') }}</li>
                     </ol>
                     <p class="fs-8 fw-bold mb-2">{{ translate('Fungsi') }} :</p>
-                    <p class="fs-8 fw-bold mb-2">Fungsi :</p>
                     <ol>
                         <li class="text-justify fs-8 fw-light lh-sm mb-1">{{ translate('Perumusan kebijakan teknis perencanaan pembangunan daerah sesuai dengan rencana strategis yang ditetapkan pemerintah daerah.') }}</li>
                         <li class="text-justify fs-8 fw-light lh-sm mb-1">{{ translate('Perencanaan, pembinaan dan pengendalian kebijakan teknis bidang perencanaan, pengendalian dan evaluasi pembangunan daerah, pemerintahan dan pembangunan manusia, perekonomian dan sumber daya alam serta infrastruktur dan kewilayahan.') }}</li>
