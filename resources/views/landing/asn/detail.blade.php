4320b5da0159cef870d978871ad9c6461ed58ad0
andypratama3 <andypratama1211@gmail.com>
2025-01-20 05:15:57 +0800

    translate page: Asn

--- resources/views/landing/asn/detail.blade.php
+++ resources/views/landing/asn/detail.blade.php
@@ -58,7 +58,7 @@
                                     {{ translate('jabatan') }}
                                 </p>
                                 <p class="text-grey fs-8 lh-sm m-0 p-0">
-                                    {{ translate($pejab->jabatan)  }} / {{ translate($kepala->jabatan_lainnya ?? '') }}
+                                    {{ translate($pejab->jabatan)  }} / {{ translate($pejab->jabatan_lainnya ?? '') }}
                                 </p>
                             </div>
                             <div class="asn-team-leader d-flex flex-column align-items-center justify-content-center border-top m-0 p-0 pt-3 pb-2 gap-2">
