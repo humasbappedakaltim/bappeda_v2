f1b57e5b7d21a3ef440fd0581ffe9c85fb13ca50
andypratama3 <andypratama1211@gmail.com>
2025-01-25 23:26:09 +0800

    feat: Update Pio Validation

--- app/Http/Controllers/PioController.php
+++ app/Http/Controllers/PioController.php
@@ -33,9 +33,9 @@ class PioController extends Controller
         if ($request->hasFile('ktp')) {
             $file = $request->file('ktp');
 
-            if (!in_array($file->getMimeType(), ['image/jpeg', 'image/png', 'image/jpg'])) {
-                return back()->withErrors(['ktp' => 'The uploaded file must be a valid image.']);
-            }
+            // if (!in_array($file->getMimeType(), ['image/jpeg', 'image/png', 'image/jpg'])) {
+            //     return back()->withErrors(['ktp' => 'The uploaded file must be a valid image.']);
+            // }
 
             $upload_path = public_path('storage/docs-pio/ktp/');
             $file_name_ktp = 'Ktp_' . Str::slug($request->name) . '_' . date('YmdHis') . '.' . $file->getClientOriginalExtension();
@@ -46,9 +46,9 @@ class PioController extends Controller
         if ($request->hasFile('akta_kelahiran')) {
             $file = $request->file('akta_kelahiran');
 
-            if (!in_array($file->getMimeType(), ['image/jpeg', 'image/png', 'image/jpg'])) {
-                return back()->withErrors(['akta_kelahiran' => 'The uploaded file must be a valid image.']);
-            }
+            // if (!in_array($file->getMimeType(), ['image/jpeg', 'image/png', 'image/jpg'])) {
+            //     return back()->withErrors(['akta_kelahiran' => 'The uploaded file must be a valid image.']);
+            // }
 
             $upload_path = public_path('storage/docs-pio/akta/');
             $file_name_akta = 'Akta_' . Str::slug($request->name) . '_' . date('YmdHis') . '.' . $file->getClientOriginalExtension();
