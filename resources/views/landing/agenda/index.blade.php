dbabca5c0f2453636129e59a03e0ddb09ce38b27
andypratama3 <andypratama1211@gmail.com>
2025-01-29 18:54:17 +0800

    feat: Agenda Update head Icon

--- resources/views/landing/agenda/index.blade.php
+++ resources/views/landing/agenda/index.blade.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="stylesheet" href="{{ asset('assets/libs/OwlCarousel-2/dist/assets/owl.carousel.min.css')}}">
     <link rel="stylesheet" href="{{ asset('assets/css/iconfont/tabler-icons.css')}}">
+    <link rel="shortcut icon" href="{{ asset('asset_dashboard/img/photos/logo_head.png') }}" />
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!-- boostrap  icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
